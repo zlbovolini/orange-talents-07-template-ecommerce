b8e2a2789eaab500b428f3e95d7835fa24193d4c
Lucas Bovolini <lucas.bovolini@zup.com.br>
2021-08-04 14:00:49 -0300

    Configuracao inicial do projeto

---
+++ src/main/java/com/github/zlbovolini/mercadolivre/MercadoLivreApplication.java
@@ -0,0 +1,13 @@
+package com.github.zlbovolini.mercadolivre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoLivreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoLivreApplication.class, args);
+	}
+
+}
